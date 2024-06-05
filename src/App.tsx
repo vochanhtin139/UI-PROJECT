210ebf4b7aa90c08114ae74ee86836887d64c49c
Võ Chánh Tín <tin.vo.4767@outlook.com>
2024-06-06 01:25:41 +0700

    working on header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navbar />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
